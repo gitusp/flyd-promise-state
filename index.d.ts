2d76f1590cf5fff351d17bf100a222383caba137
gitusp <usui@lemo.is>
2019-02-18 03:10:16 +0900

    Fix type definition

--- index.d.ts
+++ index.d.ts
@@ -1,16 +1,34 @@
+export type PromiseStatePending = { state: "pending" };
+
+export type PromiseStateFulfilled<T> = { state: "fulfilled"; value: T };
+
+export type PromiseStateRejected = { state: "rejected"; value: Error };
+
 export type PromiseState<T> =
-  | { state: "pending" }
-  | { state: "fulfilled"; value: T }
-  | { state: "rejected"; reason: Error };
+  | PromiseStatePending
+  | PromiseStateFulfilled<T>
+  | PromiseStateRejected;
 
 export declare function toStateStream<T>(
   promise: Promise<T> | PromiseLike<T>
 ): flyd.Stream<PromiseState<T>>;
 
-export declare function toValueStream<T>(
-  stream: flyd.Stream<PromiseState<T>>
+export declare function filterFulfilled<
+  T,
+  U extends PromiseState<T>,
+  V extends PromiseStateFulfilled<T>
+>(stream: flyd.Stream<U>): flyd.Stream<V>;
+
+export declare function filterRejected<
+  T,
+  U extends PromiseState<T>,
+  V extends PromiseStateRejected
+>(stream: flyd.Stream<U>): flyd.Stream<V>;
+
+export declare function toValueStream<T, U extends PromiseState<T>>(
+  stream: flyd.Stream<U>
 ): flyd.Stream<T>;
 
-export declare function toReasonStream<T>(
-  stream: flyd.Stream<PromiseState<T>>
+export declare function toReasonStream<T, U extends PromiseState<T>>(
+  stream: flyd.Stream<U>
 ): flyd.Stream<Error>;
